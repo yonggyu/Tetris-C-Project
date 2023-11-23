d73f07d16322b8a9e20af74a923d27730fdfee20
Younggyu <yonggyu1234@naver.com>
2023-11-23 13:21:17 +0900

    테트리스 코드

---
+++ src/tetris.c
@@ -0,0 +1,221 @@
+#define _CRT_NONSTDC_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+
+#define randomize() srand((unsigned)time(NULL))
+#define random(n) (rand() % (n))
+#define delay(n) Sleep(n)
+#define clrscr() system("cls")
+#define gotoxy(x,y) { COORD Cur = {x, y}; \
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),Cur);}
+#define showcursor(bShow) { CONSOLE_CURSOR_INFO CurInfo = {20, bShow}; \
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&CurInfo); }
+
+
+enum { ESC = 27, LEFT = 75, RIGHT = 77, UP = 72, DOWN = 80 };
+#define putsxy(x, y, s) {gotoxy(x, y);puts(s);}
+#define BX 5
+#define BY 1
+#define BW 10
+#define BH 20
+
+void DrawScreen();
+BOOL ProcessKey();
+void PrintBrick(BOOL Show);
+int GetAround(int x, int y, int b, int r);
+BOOL MoveDown();
+void TestFull();
+
+struct Point {
+	int x, y;
+};
+struct Point Shape[][4][4] = {
+	{ { 0,0,1,0,2,0,-1,0 },{ 0,0,0,1,0,-1,0,-2 },{ 0,0,1,0,2,0,-1,0 },{ 0,0,0,1,0,-1,0,-2 } },
+	{ { 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 },{ 0,0,1,0,0,1,1,1 } },
+	{ { 0,0,-1,0,0,-1,1,-1 },{ 0,0,0,1,-1,0,-1,-1 },{ 0,0,-1,0,0,-1,1,-1 },{ 0,0,0,1,-1,0,-1,-1 } },
+	{ { 0,0,-1,-1,0,-1,1,0 },{ 0,0,-1,0,-1,1,0,-1 },{ 0,0,-1,-1,0,-1,1,0 },{ 0,0,-1,0,-1,1,0,-1 } },
+	{ { 0,0,-1,0,1,0,-1,-1 },{ 0,0,0,-1,0,1,-1,1 },{ 0,0,-1,0,1,0,1,1 },{ 0,0,0,-1,0,1,1,-1 } },
+	{ { 0,0,1,0,-1,0,1,-1 },{ 0,0,0,1,0,-1,-1,-1 },{ 0,0,1,0,-1,0,-1,1 },{ 0,0,0,-1,0,1,1,1 } },
+	{ { 0,0,-1,0,1,0,0,1 },{ 0,0,0,-1,0,1,1,0 },{ 0,0,-1,0,1,0,0,-1 },{ 0,0,-1,0,0,-1,0,1 } },
+};
+
+enum { EMPTY, BRICK, WALL };
+char arTile[3][4] = { ". ","■","□" };
+int board[BW + 2][BH + 2];
+int nx, ny;
+int brick, rot;
+
+int main()
+{
+	int nFrame, nStay;
+	int x, y;
+
+	showcursor(FALSE);
+	randomize();
+	clrscr();
+
+	// 가장자리는 벽, 나머지는 빈 공간으로 초기화한다.
+	for (x = 0; x < BW + 2; x++) {
+		for (y = 0; y < BH + 2; y++) {
+			if (y == 0 || y == BH + 1 || x == 0 || x == BW + 1) {
+				board[x][y] = WALL;
+			}
+			else {
+				board[x][y] = EMPTY;
+			}
+		}
+	}
+	DrawScreen();
+	nFrame = 20;
+
+	// 전체 게임 루프
+	for (; 1;) {
+		brick = random(sizeof(Shape) / sizeof(Shape[0]));
+		nx = BW / 2;
+		ny = 3;
+		rot = 0;
+		PrintBrick(TRUE);
+
+		if (GetAround(nx, ny, brick, rot) != EMPTY) break;
+
+		// 벽돌 하나가 바닥에 닿을 때까지의 루프
+		nStay = nFrame;
+		for (; 2;) {
+			if (--nStay == 0) {
+				nStay = nFrame;
+				if (MoveDown()) break;
+			}
+			if (ProcessKey()) break;
+			delay(1000 / 20);
+		}
+	}
+	clrscr();
+	putsxy(30, 12, "G A M E  O V E R");
+	showcursor(TRUE);
+}
+
+void DrawScreen()
+{
+	for (int x = 0; x < BW + 2; x++) {
+		for (int y = 0; y < BH + 2; y++) {
+			putsxy(BX + x * 2, BY + y, arTile[board[x][y]]);
+		}
+	}
+
+	putsxy(50, 3, "Tetris Ver 1.0");
+	putsxy(50, 5, "좌우:이동, 위:회전, 아래:내림");
+	putsxy(50, 6, "공백:전부 내림");
+}
+
+BOOL ProcessKey()
+{
+	if (kbhit()) {
+		int ch = getch();
+		if (ch == 0xE0 || ch == 0) {
+			ch = getch();
+			switch (ch) {
+			case LEFT:
+				if (GetAround(nx - 1, ny, brick, rot) == EMPTY) {
+					PrintBrick(FALSE);
+					nx--;
+					PrintBrick(TRUE);
+				}
+				break;
+			case RIGHT:
+				if (GetAround(nx + 1, ny, brick, rot) == EMPTY) {
+					PrintBrick(FALSE);
+					nx++;
+					PrintBrick(TRUE);
+				}
+				break;
+			case UP:
+				if (GetAround(nx, ny, brick, (rot + 1) % 4) == EMPTY) {
+					PrintBrick(FALSE);
+					rot = (rot + 1) % 4;
+					PrintBrick(TRUE);
+				}
+				break;
+			case DOWN:
+				if (MoveDown()) {
+					return TRUE;
+				}
+				break;
+			}
+		}
+		else {
+			switch (ch) {
+			case ' ':
+				while (MoveDown() == FALSE) { ; }
+				return TRUE;
+			case ESC:
+				exit(0);
+			}
+		}
+	}
+	return FALSE;
+}
+
+void PrintBrick(BOOL Show)
+{
+	for (int i = 0; i < 4; i++) {
+		gotoxy(BX + (Shape[brick][rot][i].x + nx) * 2, BY + Shape[brick][rot][i].y + ny);
+		puts(arTile[Show ? BRICK : EMPTY]);
+	}
+}
+
+int GetAround(int x, int y, int b, int r)
+{
+	int k = EMPTY;
+
+	// 벽돌이 차지한 타일 모양 중 가장 큰 값을 찾는다.
+	for (int i = 0; i < 4; i++) {
+		k = max(k, board[x + Shape[b][r][i].x][y + Shape[b][r][i].y]);
+	}
+	return k;
+}
+
+BOOL MoveDown()
+{
+	// 바닥에 닿았으면 가득찼는지 점검하고 TRUE를 리턴한다.
+	if (GetAround(nx, ny + 1, brick, rot) != EMPTY) {
+		TestFull();
+		return TRUE;
+	}
+	// 아직 공중에 떠 있으면 한칸 아래로 내린다.
+	PrintBrick(FALSE);
+	ny++;
+	PrintBrick(TRUE);
+	return FALSE;
+}
+
+void TestFull()
+{
+	// 바닥에 내려앉은 벽돌 기록
+	for (int i = 0; i < 4; i++) {
+		board[nx + Shape[brick][rot][i].x][ny + Shape[brick][rot][i].y] = BRICK;
+	}
+
+	// 수평으로 가득찬 벽돌 제거
+	for (int y = 1; y < BH + 1; y++) {
+		BOOL bFull = TRUE;
+		for (int x = 1; x < BW + 1; x++) {
+			if (board[x][y] == EMPTY) {
+				bFull = FALSE;
+				break;
+			}
+		}
+		// 한줄이 가득 찼으면 이 줄을 제거한다.
+		if (bFull) {
+			for (int ty = y; ty > 1; ty--) {
+				for (int x = 1; x < BW + 1; x++) {
+					board[x][ty] = board[x][ty - 1];
+				}
+			}
+			DrawScreen();
+			delay(200);
+		}
+	}
+}
